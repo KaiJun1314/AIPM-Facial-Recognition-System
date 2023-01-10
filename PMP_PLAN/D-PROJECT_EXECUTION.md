70d56d3c0b05b396acf3565ac3dd67a86e1d44c5
KaiJun <kjwong1314@gmail.com>
2023-01-10 23:14:51 +0800

    Update Code

--- PMP_PLAN/D-PROJECT_EXECUTION.md
+++ PMP_PLAN/D-PROJECT_EXECUTION.md
@@ -5,6 +5,7 @@ Flowchart Design:\
 <img src="assets/FlowChart.png" width="60%">
 
 ### Description of the project coding and implementation
+Libraries and Packages required:
 ```
 deepface==0.0.75
 Flask==2.2.2
@@ -12,6 +13,17 @@ numpy==1.24.0
 opencv-python==4.6.0.66
 Flask-Cors==3.0.10
 ```
+
+Facial Recognition: Firstly, we load our model from the pre-trained which created by sergiomsilva.From our Github repository, two files: wpod-net.h5 and wpod-net.json canbe found. Notifications "loading model successfully..." should be get after this step.
+
+Subsequently, we create a function name preprocess_image to read and pre-process our plate images. This function basically reads the parsing image, converts it to RGB (line 3) and normalizes the image data to 0–1 range so it can be compatible with matplotlib. Additionally, we can set resize = True to resize all images to same dimension of (width = 224, height = 224) for visualizing purpose in the next step. 
+
+In this step, we visualize our vehicle dataset. This dataset contains of 20 vehicle images with plate acquired from 10 different countries (Germany, Vietnam, Japan, Thailand, Saudi, Russia, Korea, Usa, India, China). The following block of code will display plate images and their country names in a figure containing of 5 columns and 4 rows.
+
+Now, function named get_plate which processes the raw image is written, and is sent to our model and return the plate image (LpImg) and its coordinates (cor). If there is no plate founded, the program would warn with an error “No License plate is founded!”. 
+
+We draw a bounding box with obtained coordinates of deteced plate. 
+We perform get_plate function to all vehicle images and plot the returend plate images. 
 
 ### Project Result
 Result using Web API:\
--- src/app.py
+++ src/app.py
@@ -2,13 +2,62 @@ from flask import Flask, request
 import base64
 import cv2
 import numpy as np
-from deepface import DeepFace
-from deepface.basemodels import Facenet
 from flask_cors import CORS
+from PIL import Image
+from numpy import asarray, expand_dims
+from scipy.spatial.distance import cosine
+from mtcnn.mtcnn import MTCNN
+from keras_vggface.vggface import VGGFace
+from keras_vggface.utils import preprocess_input
+from datetime import datetime
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pylab as plt
+import os
+import pandas as pd
 
+global names
+global database_embedding
 app = Flask(__name__)
 CORS(app)
 
+def extract_face(filename, required_size=(224, 224)):
+	# load image from file
+	pixels = plt.imread(filename)
+	# create the detector, using default weights
+	detector = MTCNN()
+	try: 
+		# detect faces in the image
+		results = detector.detect_faces(pixels)
+		# extract the bounding box from the first face
+		x1, y1, width, height = results[0]['box']
+	except:
+		raise ValueError("No Face Detected")
+	
+	x2, y2 = x1 + width, y1 + height
+	# extract the face
+	face = pixels[y1:y2, x1:x2]
+	# resize pixels to the model size
+	image = Image.fromarray(face)
+	image = image.resize(required_size)
+	face_array = asarray(image)
+	return face_array
+
+# extract faces and calculate face embeddings for a list of photo files
+def get_embeddings(filenames):
+	# extract faces
+	faces = [extract_face(f) for f in filenames]
+	# convert into an array of samples
+	samples = asarray(faces, 'float32')
+	# prepare the face for the model, e.g. center pixels
+	samples = preprocess_input(samples, version=2)
+	# create a vggface model
+	model = VGGFace(model='resnet50', include_top=False, input_shape=(224, 224, 3), pooling='avg')
+	# perform prediction
+	yhat = model.predict(samples)
+	return yhat
+
+# convert base64 image data to cv2
 def base64_to_cv2(image_base64):
     """base64 image to cv2"""
     idx = image_base64.find('base64,')
@@ -19,23 +68,35 @@ def base64_to_cv2(image_base64):
     image_cv2 = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
     return image_cv2
 
+# detect person and get name
 def detect_person(input_path):
-    model = Facenet.loadModel()
-    try:
-        df = DeepFace.find(img_path = "image.jpg", db_path = "database", 
-            model_name="Facenet", model = model, distance_metric= 'cosine', detector_backend='ssd')
-    except ValueError: 
-        return "Face could not be detected"
+	try:
+		pixels = extract_face(input_path)
+		plt.imshow(pixels)
+		plt.axis("off")
+		record_name  = datetime.now().strftime("%m%d%Y%H%M%S")
+		path = os.path.join("record", record_name)
+		plt.savefig(path)
+	except (ValueError): 
+		return "No Face Detected"
 
-    df = df.sort_values(by=['Facenet_cosine'])
-    if len(df) > 0: 
-        if df.iloc[0].Facenet_cosine <= 0.5:
-            name = df.iloc[0].identity.split('\\')[1].split('/')
-            return "Welcome " + name[0]
-        else :
-            return "No Authorized Personnel detected"
-    else:
-        return "No Authorized Personnel detected"
+	pixels = pixels.astype('float32')
+	samples = expand_dims(pixels, axis=0)
+	samples = preprocess_input(samples, version=2)
+	model = VGGFace(model='resnet50', include_top=False, input_shape=(224, 224, 3), pooling='avg')
+	yhat = model.predict(samples)
+
+	cosine_similarity = [cosine(yhat[0],item) for item in database_embedding]
+	database = pd.DataFrame(data = {"name" : names, "cosine" : cosine_similarity})
+	database = database.sort_values(by=['cosine'], ignore_index=True)
+
+	if database['cosine'][0] <= 0.5:
+		name = os.path.splitext(database['name'][0])[0]
+		name = name.replace("_", " ").split(" ")
+		name = " ".join( i.capitalize() for i in name)
+		return "Welcome " + name
+	else:
+		return "No Authorized Personnel detected"
 
 @app.route("/recognition", methods=['POST'])
 def recognition():
@@ -45,5 +106,7 @@ def recognition():
         cv2.imwrite(input_path, input)
         return detect_person(input_path)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+names = os.listdir("database")
+database_path = [os.path.join("database", image) for image in names]
+database_embedding = get_embeddings(database_path)
+app.run(debug=True)
